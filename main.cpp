a18ed827bb67ec5d50a234cefae6c3034c499bc9
Niko Junkala <baabelfish@gmail.com>
2013-05-24 20:09:13 +0300

    Code wrapped to 80 cols

--- main.cpp
+++ main.cpp
@@ -26,7 +26,8 @@ void init() {
 
 struct pam_response *reply;
 
-int function_conversation(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
+int function_conversation(int num_msg, const struct pam_message **msg
+        , struct pam_response **resp, void *appdata_ptr) {
     *resp = reply;
     return PAM_SUCCESS;
 }
@@ -41,7 +42,8 @@ bool authenticate(const std::string& password) {
         return false;
     }
 
-    if (pam_start("common-auth", p, &local_conversation, &handle) != PAM_SUCCESS) {
+    if (pam_start("common-auth", p, &local_conversation, &handle) 
+            != PAM_SUCCESS) {
         return false;
     }
 
